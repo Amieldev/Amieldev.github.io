e57e73ea6d80cd07c3412845e12320c87633fcb6
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-03-17 12:40:04 +0300

    Fixed

--- physics-calculator/search.js
+++ physics-calculator/search.js
@@ -19,7 +19,7 @@ var text=text2.toLowerCase();
 	if(text=="gravity"){
 	window.location="gravity.html";
 }
-	if(text=="pathagorean theorem"||text=="hypotenuse"){
+	if(text=="pythagorean theorem"||text=="hypotenuse"){
 	window.location="hypo.html";
 }
 	if(text=="e=mc2"||text=="mass energy equivalence"){
