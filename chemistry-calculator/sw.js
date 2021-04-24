f713ca65ce277f680fec5522190ec8690788771b
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-04-24 13:29:59 +0300

    PWA for Chem

---
+++ chemistry-calculator/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install",e=>{
+    e.waitUntil(
+        caches.open("static").then(cache=>{
+            return cache.addAll(["atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","logo512.png","logo192.png"])
+        })
+    )
+});
+self.addEventListener("fetch",e=>{
+    console.log(`Fetch request for:${e.request.url}`);
+})
