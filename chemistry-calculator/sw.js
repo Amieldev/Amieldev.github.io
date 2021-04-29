e5ee2b8928a8bc26432748e9f495d9430e1df07d
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-04-29 15:34:06 +0300

    try chem

--- chemistry-calculator/sw.js
+++ chemistry-calculator/sw.js
@@ -1,10 +1,12 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
-            return cache.addAll(["index.html","atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","logo512.png","logo192.png"])
+            return cache.addAll(["index.html","atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","logo512.png","logo192.png"]);
         })
     )
 });
 self.addEventListener("fetch",e=>{
-    console.log(`Fetch request for:${e.request.url}`);
-})
+    e.respondWith(
+        caches.match(e.request)||fetch(e.request)
+        )
+});
