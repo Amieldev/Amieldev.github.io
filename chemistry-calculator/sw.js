f5529933665b3cc7b721719f55913714fda08402
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-04-29 15:42:10 +0300

    Fix

--- chemistry-calculator/sw.js
+++ chemistry-calculator/sw.js
@@ -7,6 +7,8 @@ self.addEventListener("install",e=>{
 });
 self.addEventListener("fetch",e=>{
     e.respondWith(
-        caches.match(e.request)||fetch(e.request)
-        )
-});
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+})
