6e49557ab2baea09ffba38ef6f75a7b33f7eba31
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-05-02 11:45:12 +0300

    Fix Chem Sw

--- chemistry-calculator/sw.js
+++ chemistry-calculator/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
-            return cache.addAll(["index.html","atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","logo512.png","logo192.png"]);
+            return cache.addAll(["logo512.png"]);
         })
     )
 });
