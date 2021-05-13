e3e14f56b89bcd246631749af72e87258647336b
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-05-13 13:12:42 +0300

    Delete pwa experement

--- my-pwa/neba/index.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("sw.js").then(register =>{
-        console.log("Sw registered")
-    }).catch(error =>{
-        console.log(error)
-    });
-}
