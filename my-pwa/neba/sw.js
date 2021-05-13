2596bbff4c0a223aace69abbc1e0546af450eba7
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-05-13 12:16:03 +0300

    Fixing pwa sw update

--- my-pwa/neba/sw.js
+++ my-pwa/neba/sw.js
@@ -1,20 +1,23 @@
-const cacheName = 'static-v1';
+const staticCacheName = 'static-v1';
 
 self.addEventListener("install",e=>{
     e.waitUntil(
-        caches.open(cacheName).then(cache=>{
+        caches.open(staticCacheName).then(cache=>{
             return cache.addAll(["https://amieldev.github.io/my-pwa/neba/","astra.mp3","marsh.jpg","style.jpg","style.css"]);
         })
     )
 });
 
 
-self.addEventListener('activate', async (event) => {
-    const existingCaches = await caches.keys();
-    const invalidCaches = existingCaches.filter(c => c !== cacheName);
-    await Promise.all(invalidCaches.map(ic => caches.delete(ic)));
-
-    // do whatever else you need to...
+self.addEventListener('activate',(evt) => {
+  evt.waitUntil(
+      caches.keys().then(keys=>{
+        return Promise.all(keys
+            .filter(key=>key!==staticCacheName)
+            .map(key=>caches.delete(key))    
+        )
+      })
+  )
 
 });
 
