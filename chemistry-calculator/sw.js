e2733c96fc6e18c79a3d2a9534da9a3f41f02174
Amiel Sintayehu <amielsentayehu@gmail.com>
2021-05-23 15:14:43 +0300

    Trying update sw

--- chemistry-calculator/sw.js
+++ chemistry-calculator/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
         caches.open("static").then(cache=>{
-            return cache.addAll(["https://amieldev.github.io/chemistry-calculator/","index.html","atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","back.png","hum.png","setting.html","privacy.html","icon.png","logo512.png","setting.png","privacy.png","main.js"]);
+            return cache.addAll(["https://amieldev.github.io/chemistry-calculato","index.html","atom btn2.png","nuclie.jpg","nuclie-m.jpg","calculator.html","calculator.css","calculator.js","style.css","back.png","hum.png","setting.html","privacy.html","icon.png","logo512.png","setting.png","privacy.png","main.js"]);
         })
     )
 });
